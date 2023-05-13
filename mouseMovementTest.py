8587960f1c24ffe92818c98976e57bdfb46c38be
Ron King <ronk62@gmail.com>
2023-05-13 12:03:32 -0700

    testing win32api.SetCursorPos

--- mouseMovementTest.py
+++ mouseMovementTest.py
@@ -3,37 +3,29 @@
 from Keyboard_And_Mouse_Controls import *
 import math
 
-y_distance = 20
-x_distance = 75
+y_distance = 0
+x_distance = 30
 
 while True:
-    # input("Wait at prompt ")
-
-    # yCoord = 0
-    # xCoord = 0
-
-    # yCoord = int(input("Enter yCoord"))
-    # xCoord = int(input("Enter xCoord"))
-
-    # if yCoord == None:
-    #     break
-
+    input("Wait at prompt ")
     for i in range(1,6):
         print(i)
         time.sleep(1)
 
-    # target = [yCoord, xCoord]
 
-    # # AimMouse(target)
+    print("move mouse ")
+    for i in range(1,27):
+        print(i, x_distance)
+        # x_distance = -i
+        
+        # y_distance = -1 * y_distance
+        x_distance = -1 * x_distance
+        win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, x_distance, y_distance, 0, 0)
 
-    # offsetY, offsetX = target
-    # y_distance = -1 * (450 - offsetY)
-    # x_distance = -1 * (800 - offsetX)
-    # win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, x_distance, y_distance, 0, 0)
+        win32api.SetCursorPos((0,0))
 
-    win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, x_distance, y_distance, 0, 0)
+        time.sleep(1)
 
-    y_distance = -1 * y_distance
-    x_distance = -1 * x_distance
+    
 
 
